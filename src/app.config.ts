bbca6eccbc8d2c9fb8cc62c8962f3f8f86a34399
ahabhgk <ahabhgk@gmail.com>
2020-07-24 11:04:33 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+export default {
+  pages: [
+    'pages/my-reward/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#F6F6F9',
+    navigationBarTitleText: '重邮帮',
+    navigationBarTextStyle: 'black',
+  }
+}
---
+++ src/pages/my-reward/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from '@tarojs/components'
+
+const MyReward = () => {
+  
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+export default MyReward
