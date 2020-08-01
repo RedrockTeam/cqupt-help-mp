c841c61cf5d16c23298e49acd5cc2997398f5192
Gnocchiii <1944465209@qq.com>
2020-08-01 14:00:16 +0800

    feat(volunteer):新增登录验证信息弹窗组件

--- src/modules/volunteer/pages/entry/index.jsx
+++ src/modules/volunteer/pages/entry/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, Button, Input } from "@tarojs/components";
 import NavBack from "@/common/components/nav-back";
+import Popup from "../../components/popup/index";
 import styles from "./index.module.scss";
 
 const VolunteerEntry = () => {
   const [phone, setPhone] = useState();
   const [IdCardNum, setIdCardNum] = useState();
   const [volunteerNum, setVolunteerNUm] = useState();
+  const [showVerify, setShowVerify] = useState(false);
 
   const changePhone = (e) => {
     setPhone(e.detail.value);
@@ -20,51 +22,71 @@ const VolunteerEntry = () => {
     setVolunteerNUm(e.detail.value);
   };
 
+  const handleShowVerify = () => {
+    setShowVerify(true);
+  };
+
+  const cancelPopup = () => {
+    setShowVerify(false);
+  };
+
   return (
-    <View className={styles.wrapper}>
-      <NavBack title="志愿报名" background="#F6F6F9" />
-      <View className={styles.title}>
-        <View>欢迎使用</View>
-        <View>志愿报名系统</View>
+    <View>
+      <View className={styles.wrapper}>
+        <NavBack title="志愿报名" background="#F6F6F9" />
+        <View className={styles.title}>
+          <View>欢迎使用</View>
+          <View>志愿报名系统</View>
+        </View>
+        <View className={styles.form}>
+          <View className={styles.formLabel}>电话号码:</View>
+          <Input
+            className={styles.formInput}
+            value={phone}
+            onInput={(e) => {
+              changePhone(e);
+            }}
+          />
+          <View className={styles.formLabel}>身份证号:</View>
+          <Input
+            className={styles.formInput}
+            value={IdCardNum}
+            onInput={(e) => {
+              changeIdCardNum(e);
+            }}
+          />
+          <View className={styles.formLabel}>志愿者账号:</View>
+          <Input
+            className={styles.formInput}
+            value={volunteerNum}
+            onInput={(e) => {
+              changeVolunteerNum(e);
+            }}
+          />
+        </View>
+        <View className={styles.tips}>
+          注意：请仔细输入信息，该信息将影响
+          <Text style={{ color: "rgba(254,145,125)" }}>志愿时长</Text>
+          的统计！
+        </View>
+        <Button
+          className={
+            phone && IdCardNum && volunteerNum
+              ? styles.buttonPush
+              : styles.button
+          }
+          onClick={handleShowVerify}
+        >
+          登录
+        </Button>
       </View>
-      <View className={styles.form}>
-        <View className={styles.formLabel}>电话号码:</View>
-        <Input
-          className={styles.formInput}
-          value={phone}
-          onInput={(e) => {
-            changePhone(e);
-          }}
-        />
-        <View className={styles.formLabel}>身份证号:</View>
-        <Input
-          className={styles.formInput}
-          value={IdCardNum}
-          onInput={(e) => {
-            changeIdCardNum(e);
-          }}
-        />
-        <View className={styles.formLabel}>志愿者账号:</View>
-        <Input
-          className={styles.formInput}
-          value={volunteerNum}
-          onInput={(e) => {
-            changeVolunteerNum(e);
-          }}
-        />
-      </View>
-      <View className={styles.tips}>
-        注意：请仔细输入信息，该信息将影响
-        <Text style={{ color: "rgba(254,145,125)" }}>志愿时长</Text>
-        的统计！
-      </View>
-      <Button
-        className={
-          phone && IdCardNum && volunteerNum ? styles.buttonPush : styles.button
-        }
-      >
-        登录
-      </Button>
+      <Popup
+        visible={showVerify}
+        phone={phone}
+        IdCardNum={IdCardNum}
+        volunteerNum={volunteerNum}
+        onCancel={cancelPopup}
+      />
     </View>
   );
 };
