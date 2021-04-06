aa0e6cee93cb7897757d5dea376bbbe46f91620e
firework <123456789>
2021-04-07 00:55:11 +0800

    feat: remove repeated util func

--- src/common/helpers/date.ts
+++ src/common/helpers/date.ts
@@ -128,9 +128,3 @@ export const genSeconds = (date : string) :{begin_time: number, end_time: number
     end_time
   }
 }
-
- *
- * 时间戳转化为时:秒
- */
-export const timestampToHMString = (timestamp: number) =>
-  timestampToDayjs(timestamp - 8 * 60 * 60).format("HH:mm");
