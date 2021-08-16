90bc1adb1fe496dc0e0c977538ad52717af965f9
Cansiny0320 <1136843910@qq.com>
2021-08-16 16:42:56 +0800

    feat: 修改身份有证首页样式

--- src/modules/id/pages/index/index.jsx
+++ src/modules/id/pages/index/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Button, OpenData } from "@tarojs/components";
 import { resolvePage, navTo } from "@/common/helpers/utils";
 import NavBack from "@/common/components/nav-back";
@@ -7,11 +7,13 @@ import { useQuery } from "react-query/dist/react-query.production.min";
 import Placeholder from "@/common/components/placeholder";
 import emptyImg from "@/static/images/empty.png";
 import { getIdCardList } from "../../services";
+import SwitchHeader from "@/common/components/switchHeader";
 import styles from "./index.module.scss";
 
 const PAGE_TITLE = "身份有证";
 
 const IdIndex = () => {
+  const [active, setActive] = useState(0);
   const { data: idCardListRes, isLoading, isError } = useQuery(
     "getIdCardList",
     getIdCardList
@@ -21,42 +23,61 @@ const IdIndex = () => {
   if (isError || !idCardListRes)
     return <Placeholder title={PAGE_TITLE} isError />;
 
-  if (idCardListRes.identity_cards.length === 0) {
-    return (
-      <Empty
-        title={PAGE_TITLE}
-        detail="证件空空如也哦～"
-        suggestion="快去申请新的会员证吧"
-        onBtnClick={() => navTo({ url: resolvePage("id", "apply") })}
-        btnContent="申请证件"
-      />
-    );
-  }
+  // if (idCardListRes.identity_cards.length === 0) {
+  //   return (
+  //     <Empty
+  //       title={PAGE_TITLE}
+  //       detail="证件空空如也哦～"
+  //       suggestion="快去申请新的会员证吧"
+  //       onBtnClick={() => navTo({ url: resolvePage("id", "apply") })}
+  //       btnContent="申请证件"
+  //     />
+  //   );
+  // }
   return (
     <View className={styles.wrapper}>
       <NavBack title={PAGE_TITLE} background="#FFFFFF" />
-      {idCardListRes.identity_cards?.map((item) => (
-        <View
-          className={styles.card}
-          key={item.name + item.team_name + item.collage + item.create_time}
-        >
-          <View className={styles.top}>
-            <OpenData
-              className={styles.avator}
-              type="userAvatarUrl"
-              defaultAvatar={emptyImg}
-            />
-            <View className={styles.top_right}>
-              <View className={styles.name}>{item.name}</View>
-              <View className={styles.info}>{item.team_name}会员</View>
+      <SwitchHeader
+        active={active}
+        setActive={setActive}
+        titles={["组织", "社团"]}
+      />
+      <View>
+        {idCardListRes.identity_cards.length === 0 ? (
+          <Empty
+            style={{
+              padding: "0px",
+            }}
+            title={PAGE_TITLE}
+            detail="证件空空如也哦～"
+            suggestion="快去申请新的会员证吧"
+            btnContent=""
+          />
+        ) : (
+          idCardListRes.identity_cards?.map((item) => (
+            <View
+              className={styles.card}
+              key={item.name + item.team_name + item.collage + item.create_time}
+            >
+              <View className={styles.top}>
+                <OpenData
+                  className={styles.avator}
+                  type="userAvatarUrl"
+                  defaultAvatar={emptyImg}
+                />
+                <View className={styles.top_right}>
+                  <View className={styles.name}>{item.name}</View>
+                  <View className={styles.info}>{item.team_name}会员</View>
+                </View>
+              </View>
+              <View className={styles.footer}>
+                <View className={styles.time}> {item.create_time}</View>
+                <View className={styles.department}>学生社团管理部</View>
+              </View>
             </View>
-          </View>
-          <View className={styles.footer}>
-            <View className={styles.time}> {item.create_time}</View>
-            <View className={styles.department}>学生社团管理部</View>
-          </View>
-        </View>
-      ))}
+          ))
+        )}
+      </View>
       <Button
         className={styles.btn}
         onClick={() => navTo({ url: resolvePage("id", "apply") })}
