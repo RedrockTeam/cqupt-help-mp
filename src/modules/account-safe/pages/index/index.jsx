510295ebeac932c371aea977079fcf39a8553973
kying-star <3077006505@qq.com>
2021-10-27 21:38:59 +0800

    fix:修改密保url

--- src/modules/account-safe/pages/index/index.jsx
+++ src/modules/account-safe/pages/index/index.jsx
@@ -1,87 +1,151 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Image, Text, Button } from "@tarojs/components";
-import { useQuery, useMutation } from "react-query/dist/react-query.production.min";
-import LoadingPage from '../../components/Loding/index.jsx'
+import {
+  useQuery,
+  useMutation,
+} from "react-query/dist/react-query.production.min";
 import { resolvePage, navTo } from "@/common/helpers/utils";
-import { getQuesAndEmailState, getPasswordState } from '../../services/index.ts'
 import request from "@/common/helpers/request";
 import { redirectTo } from "@tarojs/taro";
 import enter from "@/static/images/campus-enter-icon.png";
-import styles from "./index.module.scss";
 import { getUserInfo } from "@/stores/user";
-import { useEffect } from "react";
-const AccountSafe = () => {
-    const { stuNum } = getUserInfo();
-    const [showPop, setshowPop] = useState(false);
-    const [originLoading, setoriginLoading] = useState(true)
-    const [PasswordText, setPasswordText] = useState(null)
-    const { data, isLoading, isError } = useQuery(
-        [stuNum],
-        getQuesAndEmailState
-    );
-    console.log(data, isLoading)
-    const [mutateOrigin] = useMutation(getPasswordState, {
-        onSuccess: (res) => {
-            if (res.status === 10001) {
-                setoriginLoading(false)
-                setPasswordText('当前已修改密码')
-            } else {
-                setoriginLoading(false)
-                setPasswordText('现为初始密码')
-            }
-        },
-    });
-    const handleOrigin = async () => {
-        const res = await mutateOrigin(stuNum);
-        console.log(res)
+import styles from "./index.module.scss";
+import {
+  getQuesAndEmailState,
+  getPasswordState,
+} from "../../services/index.ts";
+import LoadingPage from "../../components/Loding/index.jsx";
+
+const AccountSafe = (callback, deps) => {
+  let stuNum;
+  getUserInfo().then((e) => {
+    console.log(1111111);
+    console.log(e);
+    stuNum = e.stuNum;
+  });
+  const [showPop, setshowPop] = useState(false);
+  const [originLoading, setoriginLoading] = useState(true);
+  const [PasswordText, setPasswordText] = useState(null);
+  const { data, isLoading, isError } = useQuery(
+    [stuNum],
+    getQuesAndEmailState,
+  );
+  console.log(data, isLoading);
+  const [mutateOrigin] = useMutation(getPasswordState, {
+    onSuccess: (res) => {
+      if (res.status === 10001) {
+        setoriginLoading(false);
+        setPasswordText("当前已修改密码");
+      } else {
+        setoriginLoading(false);
+        setPasswordText("现为初始密码");
+      }
+    },
+  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleOrigin = useCallback(async () => {
+    const res = await mutateOrigin(stuNum);
+    console.log(res);
+  }, [mutateOrigin, stuNum]);
+  useEffect(() => {
+    handleOrigin();
+  }, [handleOrigin]);
+  const handelPop = () => {
+    if (data.data == null) {
+      return;
     }
-    useEffect(() => {
-        handleOrigin()
-    }, [])
-    const handelPop = () => {
-        if (!(data.data.question_is || data.data.email_is)) {
-            setshowPop(true)
-        } else { navTo({ url: resolvePage("account-safe", "change") }); }
+    if (!(data.data.question_is || data.data.email_is)) {
+      setshowPop(true);
+    } else {
+      navTo({ url: resolvePage("account-safe", "change") });
     }
-    return (
+  };
+
+  return (
+    <View>
+      {!isLoading && !originLoading ? (
         <View>
-            {(!isLoading) && (!originLoading) ? <View>
-                <View className={styles.list}>
-                    <View className={styles.change}>
-                        <Text className={styles.textName} onClick={handleOrigin}>修改密码</Text>
-                        <Text className={styles.nowState} onClick={handelPop}>{PasswordText}</Text>
-                        <Image src={enter} className={styles.enter} />
-                    </View>
-                    <View className={styles.setprotect}>
-                        <Text className={styles.textName}> 设置密保</Text>
-                        {/* disabled={data.data.question_is} disabled={data.data.email_is} */}
-                        <Text type="default" className={styles.nowState} onClick={() => navTo({ url: resolvePage("account-safe", "protect") })}>
-                            {data.data.question_is ? "已设置密保" : "未设置密保"}
-                        </Text>
-                        <Image src={enter} className={styles.enter} />
-                    </View>
-                    <View className={styles.bindemail}>
-                        <Text className={styles.textName}>绑定邮箱</Text>
-                        <Text type="default" className={styles.nowState} onClick={() => navTo({ url: resolvePage("account-safe", "bindemail") })}>
-                            {data.data.email_is ? "已绑定邮箱" : "未绑定邮箱"}
-                        </Text>
-                        <Image src={enter} className={styles.enter} />
-                    </View>
-                </View>
+          <View className={styles.list}>
+            <View className={styles.change}>
+              <Text className={styles.textName} onClick={handleOrigin}>
+                修改密码
+              </Text>
+              <Text className={styles.nowState} onClick={handelPop}>
+                {PasswordText}
+              </Text>
+              <Image src={enter} className={styles.enter} />
+            </View>
+            <View className={styles.setprotect}>
+              <Text className={styles.textName}> 设置密保</Text>
+              {/* disabled={data.data.question_is} disabled={data.data.email_is} */}
+              <Text
+                type="default"
+                className={styles.nowState}
+                onClick={() =>
+                  navTo({ url: resolvePage("account-safe", "protect") })
+                }
+              >
+                {data.data && data.data.question_is
+                  ? "已设置密保"
+                  : "未设置密保"}
+              </Text>
+              <Image src={enter} className={styles.enter} />
+            </View>
+            <View className={styles.bindemail}>
+              <Text className={styles.textName}>绑定邮箱</Text>
+              <Text
+                type="default"
+                className={styles.nowState}
+                onClick={() =>
+                  navTo({ url: resolvePage("account-safe", "bindemail") })
+                }
+              >
+                {data.data && data.data.email_is ? "已绑定邮箱" : "未绑定邮箱"}
+              </Text>
+              <Image src={enter} className={styles.enter} />
+            </View>
+          </View>
 
-                <View>
-                    <View className={styles.cover} style={showPop ? null : "display:none;"} />
-                    <View className={showPop ? styles.popWindowActive : styles.popWindow}>
-                        <View className={styles.title}>为了账号安全请先设置密保/绑定邮箱</View>
-                        <View className={styles.bindemail} onTouchEnd={true} onClick={() => navTo({ url: resolvePage("account-safe", "bindemail") })}>绑定邮箱</View>
-                        <View className={styles.setprotect} onTouchEnd={true} onClick={() => navTo({ url: resolvePage("account-safe", "protect") })}>设置密保</View>
-                        <View className={styles.cancel} onClick={() => setshowPop(false)}>取消</View>
-                    </View>
-                </View>
-
-            </View> : <LoadingPage />}
+          <View>
+            <View
+              className={styles.cover}
+              style={showPop ? null : "display:none;"}
+            />
+            <View
+              className={showPop ? styles.popWindowActive : styles.popWindow}
+            >
+              <View className={styles.title}>
+                为了账号安全请先设置密保/绑定邮箱
+              </View>
+              <View
+                className={styles.bindemail}
+                onTouchEnd
+                onClick={() =>
+                  navTo({ url: resolvePage("account-safe", "bindemail") })
+                }
+              >
+                绑定邮箱
+              </View>
+              <View
+                className={styles.setprotect}
+                onTouchEnd
+                onClick={() =>
+                  navTo({ url: resolvePage("account-safe", "protect") })
+                }
+              >
+                设置密保
+              </View>
+              <View className={styles.cancel} onClick={() => setshowPop(false)}>
+                取消
+              </View>
+            </View>
+          </View>
         </View>
-    )
-}
+      ) : (
+        <LoadingPage />
+      )}
+    </View>
+  );
+};
 
 export default AccountSafe;
--- src/modules/account-safe/services/index.ts
+++ src/modules/account-safe/services/index.ts
@@ -1,161 +1,208 @@
 import request from "@/common/helpers/request";
-import { getQuestionLists } from './dto'
+import { getQuestionLists } from "./dto";
+import { getUserInfo } from "@/stores/user";
 
-//{domain}/user/password/personal修改密码
+// {domain}/user/password/personal修改密码
 export const changPassword = ({ Origin, New }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/user-secret/christinauser/password/personal", {
-        // https://run.mocky.io/v3/c0327d69-b08a-43d5-a084-549651041b4f
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: { origin_password: Origin, new_password: New }
-    });
-//{domain}/user/bind/email/code绑定邮箱信息
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/password/personal",
+    {
+      // https://run.mocky.io/v3/c0327d69-b08a-43d5-a084-549651041b4f
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: { origin_password: Origin, new_password: New },
+    }
+  );
+// {domain}/user/bind/email/code绑定邮箱信息
 export const sentEmailMessage = (email) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/email/code", {
-        // https://run.mocky.io/v3/c320935a-1150-4320-b0be-2e59196e1491
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: { email }
-    });
-//{domain}/user/bind/email验证邮箱密码
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/email/code",
+    {
+      // https://run.mocky.io/v3/c320935a-1150-4320-b0be-2e59196e1491
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: { email },
+    }
+  );
+// {domain}/user/bind/email验证邮箱密码
 export const bindEmail = ({ emailAddress, userCode }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/email", {
-        // https://run.mocky.io/v3/6517388e-1f46-4a69-9307-79837f41d6a1
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: { email: emailAddress, code: userCode }
-    });
-//{domain}/user/bind/question设置密保问题
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/email",
+    {
+      // https://run.mocky.io/v3/6517388e-1f46-4a69-9307-79837f41d6a1
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: { email: emailAddress, code: userCode },
+    }
+  );
+// {domain}/user/bind/question设置密保问题
 export const bindProtect = ({ id, value }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/question", {
-        // https://run.mocky.io/v3/6517388e-1f46-4a69-9307-79837f41d6a1
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: { id: id, content: value }
-    });
-//{domain}/user/question密保问题信息
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/question",
+    {
+      // https://run.mocky.io/v3/6517388e-1f46-4a69-9307-79837f41d6a1
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: { id, content: value },
+    }
+  );
+// {domain}/user/question密保问题信息
 export const getQuestionList = (_key: string) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/question", {
-        // https://run.mocky.io/v3/54947ed0-55a0-49fb-bdab-839f0ed7292e
-        method: "GET"
-    });
-//{domain}/user/bind/is是否绑定信息
-export const getQuesAndEmailState = (account) =>
-    // https://run.mocky.io/v3/201b1ee2-e943-46a4-8962-4da5d9fbddc4
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/is", {
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account
-        }
-    });
-//{domain}/user/judge/origin判断是否是默认密码
-export const getPasswordState = (account) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/judge/origin", {
-        // https://run.mocky.io/v3/d1450784-9056-4697-8a10-50ce39012af0
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account
-        }
-    });
-//{domain}/user/valid/email/code发送找回密码邮箱验证码
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/question",
+    {
+      // https://run.mocky.io/v3/54947ed0-55a0-49fb-bdab-839f0ed7292e
+      method: "GET",
+    }
+  );
+// {domain}/user/bind/is是否绑定信息
+export const getQuesAndEmailState = async () => {
+  // https://run.mocky.io/v3/201b1ee2-e943-46a4-8962-4da5d9fbddc4
+  console.log(114514);
+  const { stuNum } = await getUserInfo();
+  return request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/is",
+    {
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: {
+        stu_num: stuNum,
+      },
+    }
+  );
+};
+// {domain}/user/judge/origin判断是否是默认密码
+export const getPasswordState = async () => {
+  const { stuNum } = await getUserInfo();
+  return  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/judge/origin",
+    {
+      // https://run.mocky.io/v3/d1450784-9056-4697-8a10-50ce39012af0
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num: stuNum,
+      },
+    }
+  );
+}
+// {domain}/user/valid/email/code发送找回密码邮箱验证码
 export const checkEmail = (account) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/valid/email/code", {
-        // https://run.mocky.io/v3/c320935a-1150-4320-b0be-2e59196e1491
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account
-        }
-    });
-//{domain}/user/valid/email验证找回密码邮箱验证码
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/valid/email/code",
+    {
+      // https://run.mocky.io/v3/c320935a-1150-4320-b0be-2e59196e1491
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num: account,
+      },
+    }
+  );
+// {domain}/user/valid/email验证找回密码邮箱验证码
 export const checkEmailCode = ({ email, code, account }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/valid/email", {
-        // https://run.mocky.io/v3/4d229f07-43a8-4b3e-928a-e233864a6e3e
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            email: email,
-            code: code,
-            stu_num: account
-        }
-    });
-//{domain}/user/bind/email/detail拿到学生绑定的邮箱信息
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/valid/email",
+    {
+      // https://run.mocky.io/v3/4d229f07-43a8-4b3e-928a-e233864a6e3e
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        email,
+        code,
+        stu_num: account,
+      },
+    }
+  );
+// {domain}/user/bind/email/detail拿到学生绑定的邮箱信息
 export const getEmail = (account) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/email/detail", {
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account
-        }
-    });
-//{domain}/user/password/valid修改密码(找回密码)
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/email/detail",
+    {
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num: account,
+      },
+    }
+  );
+// {domain}/user/password/valid修改密码(找回密码)
 export const changeNewPassword = ({ account, new_password, code }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/password/valid", {
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account,
-            new_password,
-            code
-        }
-    });
-//{domain}/user/bind/question/detail学生绑定的密保信息
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/password/valid",
+    {
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num: account,
+        new_password,
+        code,
+      },
+    }
+  );
+// {domain}/user/bind/question/detail学生绑定的密保信息
 export const getProtectQuestion = (account) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/bind/question/detail", {
-        // https://run.mocky.io/v3/5a095c4b-2146-474c-9a55-df116c8eb63e
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num: account
-        }
-    });
-//{domain}/user/valid/question验证密保问题(找回问题)
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/bind/question/detail",
+    {
+      // https://run.mocky.io/v3/5a095c4b-2146-474c-9a55-df116c8eb63e
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num: account,
+      },
+    }
+  );
+// {domain}/user/valid/question验证密保问题(找回问题)
 export const checkAnswer = ({ stu_num, question_id, content }) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/valid/question", {
-        // https://run.mocky.io/v3/4d229f07-43a8-4b3e-928a-e233864a6e3e
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            stu_num,
-            question_id,
-            content
-        }
-    });
-//{domain}/user/judge/password判断原密码是否正确
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/valid/question",
+    {
+      // https://run.mocky.io/v3/4d229f07-43a8-4b3e-928a-e233864a6e3e
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        stu_num,
+        question_id,
+        content,
+      },
+    }
+  );
+// {domain}/user/judge/password判断原密码是否正确
 export const judgePassword = (password) =>
-    request<getQuestionLists>("https://be-prod.redrock.cqupt.edu.cn/wxapi/user-secret/christinauser/judge/password", {
-        method: "POST",
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            password: password
-        }
-    });
-
+  request<getQuestionLists>(
+    "https://be-prod.redrock.cqupt.edu.cn/user-secret/christina/user/judge/password",
+    {
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        password,
+      },
+    }
+  );
